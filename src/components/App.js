e40e863bee0ae8df479d2bd06029fd6cd0c71c89
Zil Norvilis <zilasino27@gmail.com>
2021-06-02 14:20:35 +0200

    Fix linter offences

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Big from "big.js";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import "../styles/App.scss";
