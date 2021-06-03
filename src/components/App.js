f5f65d070aca7627ddf3280712c8988304619fee
Zil Norvilis <zilasino27@gmail.com>
2021-06-03 09:13:21 +0200

    Refactor App to class

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import '../styles/App.scss';
-import calculate from '../logic/calculate';
+import React, { Component } from "react";
+import { useState } from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import "../styles/App.scss";
+import calculate from "../logic/calculate";
 
-function App() {
-  return (
-    <>
-      <h1>Calculator</h1>
-      <Display calculate={calculate} />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick.bind(this);
+  }
+
+  handleClick = (buttonName) => {
+    const data = this.state;
+    const result = calculate(data, buttonName);
+    this.setState({
+      total: result.total,
+      next: result.next,
+      operation: result.operation,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Calculator</h1>
+        <Display calculate={calculate} />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
